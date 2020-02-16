740e8a85c463a19309a27721b454afbb373d5255
whyboris <yboris@yahoo.com>
2020-02-16 15:24:40 -0500

    compar vectors, include .map to show where error occurred

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { runEverything } from "./pipeline";
 
-const RELATIVE_IMAGE_PATH = './images2/input.jpg';
-const CURRENT_NUMBER_OF_SCREENS = 9;  // the number of chunks the image is split into (20 screenshots for example) // HARDCODED FOR NOW
+const RELATIVE_IMAGE_PATH = './images2/img.jpg';
+const CURRENT_NUMBER_OF_SCREENS = 1;  // the number of chunks the image is split into (20 screenshots for example) // HARDCODED FOR NOW
 const OUTPUT_FILE_NAME_LARGEST = './output/largest.jpg';
 const OUTPUT_FILE_NAME_STRIP = './output/facestrip.jpg';
-const GENDER = 'female';
+const GENDER = 'male';
 const MIN_FACE_HEIGHT = 50;
 
 runEverything(
--- src/vectors.ts
+++ src/vectors.ts
@@ -1,14 +1,38 @@
+const tf = require('@tensorflow/tfjs-node');
+
+const faceapi = require('face-api.js');
+
 const fs = require('fs');
 
 // this file is for experimenting with vector stuff
 
-const rawData = fs.readFileSync('./output/vectors.json');
+const rawData1 = fs.readFileSync('./output/vectors1.json');
+const rawData2 = fs.readFileSync('./output/vectors2.json');
 
-const data = JSON.parse(rawData);
+const data1 = JSON.parse(rawData1);
+const data2 = JSON.parse(rawData2);
 
-console.log(getDistance(data[0], data[1]));
-console.log(getDistance(data[0], data[2]));
-console.log(getDistance(data[1], data[2]));
+const floats = new Float32Array(data1[0]);
+
+const labeledFaceDescriptors = new faceapi.LabeledFaceDescriptors('Test', [floats]);
+
+const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.7);
+
+const wut = faceMatcher.findBestMatch(data1[1]);
+
+console.log(wut);
+
+console.log('same');
+console.log(getDistance(data1[0], data1[1]));
+console.log(getDistance(data1[0], data1[2]));
+console.log(getDistance(data1[1], data1[2]));
+
+console.log('diff 1');
+console.log(getDistance(data2[0], data2[1]));
+
+console.log('diff 2');
+console.log(getDistance(data1[0], data2[0]));
+console.log(getDistance(data1[0], data2[1]));
 
 
 
@@ -18,5 +42,5 @@ function getDistance(vector1: number[], vector2: number[]): number {
     const diff = vector1[i] - vector2[i];
     sum = sum + (diff * diff);
   }
-  return sum;
+  return Math.sqrt(sum);
 }
